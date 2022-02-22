906549c7c3fa7708f93b7624a5077c6757d83399
onlyrayona <rayspost@gmail.com>
2022-02-22 10:59:54 -0800

    Create function to repeat string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
 
-};
+let finalString = "";
+
+if (num<0){finalString='ERROR';}
+
+    else {
+        for (let i=0; i<num; i++){
+    
+        finalString += string;
+        };
+    };
+
+return finalString;
+
+}
 
 // Do not edit below this line
 module.exports = repeatString;
