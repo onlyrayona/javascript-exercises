bb70ee020f3b246ce2d304af615a0cf443f4e478
onlyrayona <rayspost@gmail.com>
2022-02-22 12:06:50 -0800

    Create function to reverse string using split, reverse, join methods

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+      let stringArray = string.split("");
+      let reversedArray = stringArray.reverse();
+      let reversedString = reversedArray.join("");
+
+    return reversedString;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
