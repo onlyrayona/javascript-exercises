23330f8afeecbc40b139fbe99b97d800a8cf26b7
onlyrayona <rayspost@gmail.com>
2022-02-23 15:47:58 -0800

    Create function using if/elseif and while loops

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,34 @@
-const sumAll = function() {
+const sumAll = function(num1,num2){
 
-};
+    let sum;
+
+    if(typeof(num1) != "number" || typeof(num2)!= "number" || num1<0 || num2<0)
+    {
+        sum = 'ERROR';
+    }
+
+    else if (num1<num2){
+        
+        sum =num1;
+
+        while(num1<num2){
+            sum += num2;
+            num2--;
+        }
+    }  
+    else if(num2<num1){
+        sum = num2;
+
+       while(num2<num1){
+            sum += num1;
+            num1--;
+        } 
+    }
+    
+    else if(num1=num2){sum=num1+num2;}
+
+return sum
+}
 
 // Do not edit below this line
 module.exports = sumAll;
