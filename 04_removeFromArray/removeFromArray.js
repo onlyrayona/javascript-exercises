245477c5756edd309cacba2aaf919ddeadcaff36
onlyrayona <rayspost@gmail.com>
2022-02-23 13:24:41 -0800

    Create function with for-loop and splice()

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
 const removeFromArray = function() {
+    
+    let badValues = Array.from(arguments);
+    let givenArray = badValues[0];
+    badValues.splice(0,1);
+
+    for (i=0; i<badValues.length; i++){
+
+        let index = givenArray.indexOf(badValues[i]);
+        if (index != -1){
+            givenArray.splice(index,1);
+        }   
+    }    
+    return givenArray;   
+
 
 };
 
